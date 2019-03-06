629b6719df9844228ac1f217e89669ebb30e9d17
Ferveloper <fmerino@clavijo.lan>
2019-03-06 09:54:35 +0100

    Functional mock-up

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const URL = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
+
 class App extends Component {
+  state = { quotes: [], quote: '', index : null, loading : true }
+
+  async componentDidMount() {
+    const response = await fetch(URL);
+    const json = await response.json();
+    const quotes = json.quotes;
+    const randomIndex = (Math.random() * (quotes.length - 1)).toFixed();
+    this.setState({quotes : quotes, quote : quotes[randomIndex], index : randomIndex, loading : false})
+  }
+
   render() {
+    const text = this.state.quote.quote;
+    const author = this.state.quote.author;
+    if (this.state.loading === true) {
+      return <div className="text">Loading...</div>
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="quote-box">
+        <div id="text">{text}</div>
+        <button id="new-quote" onClick={this.pickRandomQuote}>New Quote</button>
+        <button id="tweet-quote">Tweet Quote</button>
+        <div id="author">{author}</div>
       </div>
     );
   }
+
+  pickRandomQuote = () => {
+    var randomIndex
+    do {
+      randomIndex = (Math.random() * (this.state.quotes.length - 1)).toFixed();
+    } while (randomIndex === this.state.index)
+    const quote = this.state.quotes[randomIndex]
+    this.setState({quote : quote, index : randomIndex})
+  }
+
 }
 
 export default App;
